ba6bc3a491c1a4a26b7cfaf695acb31da8bb3a8a
DuanNguyen2907 <87711300+DuanNguyen2907@users.noreply.github.com>
2023-02-13 11:22:56 +0700

    update admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,12 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+    if resource_class == Reader
+      devise_parameter_sanitizer.permit :sign_up, keys: [:library_card_id]
+      devise_parameter_sanitizer.permit :account_update, keys: [:library_card_id]        
+    elsif resource_class == Admin 
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:dob,:phone,:image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name,:dob,:phone,:image])
+    end
   end
 end
--- app/models/admin.rb
+++ app/models/admin.rb
@@ -1,6 +1,7 @@
 class Admin < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one_attached :image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
