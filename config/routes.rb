5c059b4dec1547cc2d18dcd88de630ca53019f6f
bachhoang0606 <0606bach@gmail.com>
2023-02-13 22:24:03 +0700

    asd

--- config/routes.rb
+++ config/routes.rb
@@ -6,20 +6,31 @@ Rails.application.routes.draw do
   resources :readers
   get 'admins/show'
   devise_for :admins
+<<<<<<< HEAD
+   
+=======
+  #get '/readers/list', to: "readers#index"
   resources :readers
+>>>>>>> origin/master
   devise_for :readers, 
             controllers: {
-              sessions: 'users/sessions',
-              registration: 'users/registrations'
+              sessions: 'readers/sessions',
+              registration: 'readers/registrations'
             },
             :path => ''
+  
   resources :library_cards
   resources :books
   resources :publishers
   resources :categories
   resources :authors
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root "borrow_pays#index"
+  root "borrow_pays#index"
   #get 'home/', to: "home#index"
+<<<<<<< HEAD
+  get '/home', to: 'home#index'
+=======
+  root to: "home#index"
   #get '/home', to: 'home#index'
+>>>>>>> origin/master
 end
