e357bfdc8acfb506333cb1330a780fc5486e260e
bachhoang0606 <0606bach@gmail.com>
2023-02-13 20:10:08 +0700

    convert to nihongo

--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,6 @@ Rails.application.routes.draw do
               registration: 'users/registrations'
             },
             :path => ''
-  #resources :readers
   resources :library_cards
   resources :books
   resources :publishers
@@ -22,6 +21,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "borrow_pays#index"
   #get 'home/', to: "home#index"
-  root to: "home#index"
   #get '/home', to: 'home#index'
 end
