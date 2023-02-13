575808eb6872ef38c248ec47b961a73c16548556
hoanganh <hoanganhlhbk@gmail.com>
2023-02-13 13:52:11 +0700

    add avatar to user

--- app/models/reader.rb
+++ app/models/reader.rb
@@ -3,4 +3,11 @@ class Reader < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #attach avatar
+  has_one_attached :avatar
+
+  #validation avatar
+  # validates :avatar, file_size: { less_than_or_equal_to: 5.megabytes },
+  #           file_content_type: { allow: ['image/jpeg', 'image/png', 'image/gif'] }
 end
