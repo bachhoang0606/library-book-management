8f3180c1e17a2123e9a058f9897bd8d61d64539b
bachhoang0606 <0606bach@gmail.com>
2023-02-13 13:34:53 +0700

    add layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home/new'
   get 'home/create'
   resources :readers
+  resources :admins
   devise_for :admins
   devise_for :readers, controllers: {
     sessions: 'readers/sessions',
